83ca4007680e23a5ff68afae1c7528caa681820c
henryqi_code <henryqimcgill@gmail.com>
2016-03-26 20:22:28 -0400

    V1.2 - Adding the BLE( in process)

--- app/src/main/java/com/example/henryqi_code/micropxiaomiuuuu/MainActivity.java
+++ app/src/main/java/com/example/henryqi_code/micropxiaomiuuuu/MainActivity.java
@@ -1,31 +1,86 @@
 package com.example.henryqi_code.micropxiaomiuuuu;
 
-import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.GridView;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Delayed;
+
+@TargetApi(21)
 public class MainActivity extends AppCompatActivity {
 
-    private SeekBar speedBar,intensityBar;
-    private TextView speedBarValue,intensityBarValue;
+    //UI Section
+    private SeekBar speedBar, intensityBar;
+    private TextView speedBarValue, intensityBarValue, bleBtnText;
+    private Button bleBtn;
+    private int blScanStage;
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private int REQUEST_ENABLE_BT = 1;
+    private Handler mHandler;
+    private static  final long SCAN_PERIOD = 10000; // 10 sec
+    private BluetoothLeScanner mLEScanner;
+    private ScanSettings settings;
+    private List<ScanFilter> filters;
+    private BluetoothGatt mGatt;
+
+    private boolean isDeviceFound = false;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //BLE section:
+        mHandler = new Handler();
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)){
+            Toast.makeText(this,"BLE Not Supported",Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
 
+        //UI Section:
         // initialize the requried variables
         init();
         //speedBar
@@ -33,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         speedBarValue.setText("Current value: " + speedBar.getProgress());
         speedBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progress = -10;
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
                 progress = progressValue - 10;
@@ -48,11 +104,11 @@ public class MainActivity extends AppCompatActivity {
                 speedBarValue.setText("Current value: " + progress);
             }
         });
-
-        // intialize the required materials
+        //intensity Bar
         intensityBarValue.setText("Current value: " + intensityBar.getProgress());
         intensityBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progress = 0;
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
                 progress = progressValue;
@@ -69,7 +125,162 @@ public class MainActivity extends AppCompatActivity {
                 intensityBarValue.setText("Current value: " + progress);
             }
         });
+        //BLE Button Text
+        blScanStage = 0;
+        bleBtnText.setText(btnTextUpdate(blScanStage));// "PRESS FOR PAIRING"
+        bleBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                blScanStage = 1;
+                bleBtnText.setText(btnTextUpdate(blScanStage)); // "SEARCHING FOR DEVICE..."
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        else{
+            if(Build.VERSION.SDK_INT >= 21){
+                mLEScanner = mBluetoothAdapter.getBluetoothLeScanner();
+                settings = new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build();
+                filters = new ArrayList<ScanFilter>();
+            }
+            scanLeDevice(true);
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()){
+            scanLeDevice(false);
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
+        if(mGatt == null){
+            return;
+        }
+        mGatt.close();
+        mGatt = null;
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_ENABLE_BT){
+           if(resultCode == Activity.RESULT_CANCELED) {
+               //Bluetooth not enabled
+               finish();
+               return;
+           }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void scanLeDevice(final boolean enable){
+        if(enable){
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if(Build.VERSION.SDK_INT < 21){
+                        mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                    }else{
+                        mLEScanner.startLeScan(mLeScanCallback); ///TOCHECK
+                    }
+                }
+            },SCAN_PERIOD);
+            if (Build.VERSION.SDK_INT < 21){
+                mBluetoothAdapter.startLeScan(mLeScanCallback);
+            } else{
+                mLEScanner.startScan(filters,settings,mScanCallback);
+            }
+        }else{
+            mLEScanner.stopScan(mScanCallback);
+        }
+    }
+
+    private ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            Log.i("callbackType", String.valueOf(callbackType));
+            Log.i("result", result.toString());
+            BluetoothDevice btDevice = result.getDevice();
+            connectToDevice(btDevice);
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results){
+            for (ScanResult sr : results){
+                Log.i("ScanResult - Results", sr.toString());
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode){
+            Log.e("Scan Failed", "Error Code: " + errorCode);
+        }
+    };
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback(){
+        @Override
+        public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("onLeScan", device.toString());
+                    connectToDevice(device);
+                }
+            });
+        }
+    };
+
+    public void connectToDevice(BluetoothDevice device){
+        if(mGatt == null){
+            mGatt = device.connectGatt(this,false,gattCallback);
+            scanLeDevice(false); // will stop after the 1st device detection
+        }
+    }
+
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            Log.i("onConnectionStateChange", "status: " + status);
+            switch (newState){
+                case BluetoothProfile.STATE_CONNECTED:
+                    Log.i("gattCallback", "STATE_CONNECTED");
+                    gatt.discoverServices();
+                    break;
+                case BluetoothProfile.STATE_DISCONNECTED:
+                    Log.i("gattCallback","STATE_DISCONNECTED");
+                    break;
+                default:
+                    Log.e("gattCallback", "STATE_OTHER");
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status){
+            List<BluetoothGattService> services = gatt.getServices();
+            Log.i("onServicesDiscovered", services.toString());
+            gatt.readCharacteristic(services.get(1).getCharacteristics().get(0));
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic,int status){
+            Log.i("onCharacteristicRead", characteristic.toString());
+            gatt.disconnect();
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -77,11 +288,94 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-    private void init(){
-        speedBar = (SeekBar)findViewById(R.id.speedBar);
-        speedBarValue = (TextView)findViewById(R.id.speedCurrent);
-        intensityBar = (SeekBar)findViewById(R.id.intensityBar);
-        intensityBarValue = (TextView)findViewById(R.id.intensityCurr);
+
+    /**
+     * UI-initialization
+     */
+    private void init() {
+        speedBar = (SeekBar) findViewById(R.id.speedBar);
+        speedBarValue = (TextView) findViewById(R.id.speedCurrent);
+
+        intensityBar = (SeekBar) findViewById(R.id.intensityBar);
+        intensityBarValue = (TextView) findViewById(R.id.intensityCurr);
+
+        bleBtn = (Button) findViewById(R.id.bleBtn);
+        bleBtnText = (TextView) findViewById(R.id.bleBtnText);
     }
 
+
+    /**
+     * @Method: provide a message to display
+     * @param blScanStage
+     * @return String: message to display
+     */
+    private String btnTextUpdate(int blScanStage){
+        String output = "";
+        switch(blScanStage){
+            case 0 :
+                output = "PRESS FOR PAIRING";
+                break;
+
+            case 1:
+                output = "SEARCHING FOR DEVICE...";
+                break;
+
+            case 2:
+                output = "DEVICE FOUND! PAIRING...";
+                break;
+
+            case 3:
+                output = "CONNECTED!";
+                break;
+        }
+        return output;
+    }
+
+    private void bleUpdate(int blScanStage){
+        switch(blScanStage){
+            case 0:
+
+        }
+    }
+    /*
+    Google System-Defined APIs
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.henryqi_code.micropxiaomiuuuu/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.henryqi_code.micropxiaomiuuuu/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
